e4c5722e78df6fa7f4bc8c521340735e2266c5d6
Chris Mellor <cmellor@gmail.com>
2022-10-24 23:45:10 +0100

    chore: Use Laravel defaults for publishing

--- src/RatingServiceProvider.php
+++ src/RatingServiceProvider.php
@@ -2,22 +2,38 @@
 
 namespace Cjmellor\Rating;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Cjmellor\Rating\View\Components\Rating;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
 
-class RatingServiceProvider extends PackageServiceProvider
+class RatingServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register()
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('rating')
-            ->hasConfigFile()
-//            ->hasViews()
-            ->hasMigration(migrationFileName: 'create_rating_table');
+        //
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/rating.php' => config_path('rating.php'),
+        ], 'rating-config');
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rating');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_ratings_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_ratings_table.php'),
+        ], 'rating-migrations');
+
+        $this->loadViewComponentsAs('show-rating', [
+            Rating::class,
+        ]);
+
+        $this->publishes([
+            __DIR__.'/../src/View/Components' => app_path('View/Components'),
+            __DIR__.'/../resources/views/components' => resource_path('views/components'),
+        ], 'rating-component');
+
+        Blade::component('show-rating', Rating::class);
     }
 }
